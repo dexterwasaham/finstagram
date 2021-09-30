dc5c14a71ab5375a3ae53d8be5a7072584b06f33
dexterwasaham <seymour948@gmail.com>
2021-09-30 03:23:14 +0000

    add ruby

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,51 @@
+def humanized_time_ago(number_of_minutes)
+    if number_of_minutes >= 60
+        number_of_hours = (number_of_minutes / 60.0).round(2)
+        "#{number_of_hours} hours ago"
+    else
+        "#{number_of_minutes} minutes ago"
+    end
+end        
+
 get '/' do
-    File.read(File.join('app/views', 'index.html'))
-end
-    
+    finstagram_post_shark = {
+        username: "sharky_j",
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naserca.com/images/shark.jpg",
+        humanized_time_ago: humanized_time_ago(140),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "sharky_j",
+            text: "Out for the long weekend... too embarrassed to show y'all the beach bod!"
+        }]
+    }
+
+    finstagram_post_whale = {
+        username: "kirk_whalum",
+        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+        photo_url: "http://naserca.com/images/whale.jpg",
+        humanized_time_ago: humanized_time_ago(65),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+        username: "kirk_whalum",
+        text: "#weekendvibes"
+        }]
+    }
+
+    finstagram_post_marlin = {
+        username: "marlin_peppa",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        photo_url: "http://naserca.com/images/marlin.jpg",
+        humanized_time_ago: humanized_time_ago(190),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+        username: "marlin_peppa",
+        text: "lunchtime! ;)"
+        }]
+    }
+
+    [finstagram_post_shark, finstagram_post_whale, finstagram_post_marlin].to_s
+end    
